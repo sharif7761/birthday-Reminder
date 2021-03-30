eab2cb0db2a683074055ead635162ad854b26bd3
sharif7761 <sharif7761@gmail.com>
2021-03-31 01:01:35 +0600

    added refresh button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
 
-  const [people, setPeople] = useState(data)
+    const [people, setPeople] = useState(data)
 
-  return (
-      <main>
-        <section className="container">
-          <h3>{people.length} birthdays today</h3>
-          <List people={people} />
-          <button onClick={() => setPeople([])}>Clear</button>
-        </section>
-      </main>
-  )
+    return (
+        <main>
+            <section className="container">
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                {
+                    people.length > 0 ?
+                        <button onClick={() => setPeople([])}>Clear</button>
+                        :
+                        <button onClick={() => setPeople(data)}>Refresh</button>
+                }
+            </section>
+        </main>
+    )
 }
 
 export default App;
